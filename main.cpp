cf03688fa764d61130f59bb970a7691e8a743a5e
chm15937 <82330736+chm15937@users.noreply.github.com>
2021-04-12 01:40:33 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  // TODO: change this to actual program.
+  cout << "000 1\n"
+          "001 0\n"
+          "010 0\n"
+          "011 1\n"
+          "100 0\n"
+          "101 1\n"
+          "110 1\n"
+          "111 0\n";
+
+  return 0;
+}
