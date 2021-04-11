729e33b76e8172c46732eade31a5ee4fbc803676
chm15937 <ckals15937@unist.ac.kr>
2021-04-12 02:17:17 +0900

    Completed the project1.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char *argv[])
 {
-  // TODO: change this to actual program.
-  cout << "000 1\n"
-          "001 0\n"
-          "010 0\n"
-          "011 1\n"
-          "100 0\n"
-          "101 1\n"
-          "110 1\n"
-          "111 0\n";
+	int n = atoi(argv[1]);
+	for(int decimal = 0; decimal < pow(2, n); decimal++)
+	{
+		int binary[n] = {0};
+		int position = 0;
+		int decimal_ = decimal;
+		while (1)
+		{
+			binary[position] = decimal_ % 2;
+			decimal_ /= 2;
 
-  return 0;
+			position++;
+			
+			if (decimal_ == 0)
+			break;
+		}
+		
+		for (int i = n - 1; i >= 0; i--)
+		 	cout << binary[i];
+		
+		cout << " ";
+
+		int count = 0;
+		for (int i = n - 1; i >= 0; i--)
+		{
+			if (binary[i] == 1)
+				count++;
+		}
+		if (count % 2 == 0)
+			cout << 1;
+		else
+			cout << 0;
+
+		cout << endl;
+	}
+	return 0;
 }
+
+
